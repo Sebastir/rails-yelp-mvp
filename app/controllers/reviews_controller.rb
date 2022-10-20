cf3114abc02f4fe22d4930a65e1a00f7781706e6
Seb Rojas <src.py@icloud.com>
2022-10-20 20:04:28 +0200

    added review feature

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,17 +8,17 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant), notice: "Restaurant was successfully created."
-    # else
-    #   render :new, status: :unprocessable_entity
-    #end
+    if @review.save
+      redirect_to restaurant_path(@restaurant), notice: "Restaurant was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 
   def set_restaurant
